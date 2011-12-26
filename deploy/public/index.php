4b3d3bb7dde50fd9717d75b645a3a76f5cc4552f
Pete X. Graham <petegraham1@gmail.com>
2011-12-26 14:53:37 +0000

    initial site build

---
+++ deploy/public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="The Official Gunt website">
+		<meta name="author" content="Pete X. Graham">
+		
+		<title>GUNT - Bleeding Edge Digital Creative Campaigns</title>
+		<link rel="stylesheet" href="/_includes/css/site.css">
+	</head>
+	
+	<body>
+		<h1>GUNT</h1>
+		<p>So future it hurts.</p>
+	
+		<!--
+		add sounds 
+		logo should "land"
+		text transition in from left and right
+		-->
+	</body>
+</html>
